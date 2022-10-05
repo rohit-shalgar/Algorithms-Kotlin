00a8f574ba7ec5d6f9f2cb87dbde0e8aa4197b29
rohit-shalgar <learn.rohits@gmail.com>
2022-10-05 16:21:50 -0500

    smallest diff

--- src/main/kotlin/arrays/Program.kt
+++ src/main/kotlin/arrays/Program.kt
@@ -1,5 +1,7 @@
 package arrays
 
+import kotlin.math.abs
+
 class Program {
 
     fun threeNumberSum(array: MutableList<Int>, targetSum: Int): List<List<Int>> {
@@ -15,5 +17,30 @@ class Program {
 
         }
         return combinations
+    }
+
+    fun smallestDifference(arrayOne: MutableList<Int>, arrayTwo: MutableList<Int>): List<Int> {
+        // Write your code here.
+        arrayOne.sort()
+        arrayTwo.sort()
+        var currentLowestDiff = Int.MAX_VALUE
+        val tempValues = intArrayOf(0, 0)
+        var arrayOneIdx = 0
+        var arrayTwoIdx = 0
+        while (arrayOneIdx < arrayOne.size && arrayTwoIdx < arrayTwo.size) {
+            val eleOne = arrayOne[arrayOneIdx]
+            val eleTwo = arrayTwo[arrayTwoIdx]
+            val diff = abs(eleOne - eleTwo)
+            if (diff == 0) return listOf(eleOne, eleTwo)
+            else if (diff < currentLowestDiff) {
+                currentLowestDiff = diff
+                tempValues[0] = eleOne
+                tempValues[1] = eleTwo
+
+            }
+            if (eleOne < eleTwo) arrayOneIdx++
+            else arrayTwoIdx++
+        }
+        return tempValues.toList()
     }
 }
